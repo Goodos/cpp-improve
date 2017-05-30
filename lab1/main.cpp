3803003711bb38be98fc1e08019d5d5941797d87
Goodos <crazyshadow876@gmail.com>
2017-05-30 21:34:31 +0300

    try

---
+++ lab1/main.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    //setlocale(LC_ALL, "Russian");
+    string input,output,s,s_sub; // сюда будем класть считанные строки
+
+    cout << "take me input file name: ";
+    getline(cin,input);
+    cout << "take me output file name: ";
+    getline(cin,output);
+
+    ifstream file_in("D:\\" + input); // файл из которого читаем
+
+    if (!file_in)
+        cout << "can't open file for read";
+    else
+    {
+        while(!file_in.eof())
+        {
+            file_in >> s;
+            if (s == "that" || s == "which")
+                s_sub += ',' + s + ' ';
+            else
+                s_sub += s + ' ';
+        }
+
+        file_in.close(); // обязательно закрываем файл что бы не повредить его
+
+        ofstream file_out ("D:\\" + output);
+
+        if (!file_out.is_open())
+            cout << "can't open file for write";
+        else
+        {
+            file_out << s_sub << endl;
+            file_out.close();
+        }
+        cout << s_sub << endl; // выводим на экран
+
+    }
+
+    return 0;
+}
+
